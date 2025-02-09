b545b18bf9916582faf5c2f97a461e7de3ef5afa
k-seltzer <ksetser@ufl.edu>
2025-02-09 10:05:44 -0500

    Missing Networking Script.

---
+++ driftlock/scenes/worlds/demo.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+var multiplayer_peer = ENetMultiplayerPeer.new()
+
+const PORT = 9999
+const ADDRESS = "localhost" # Local for developemnt, TODO: Implement UPnP
+
+#TODO: Implement Multiplayer Menus
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
