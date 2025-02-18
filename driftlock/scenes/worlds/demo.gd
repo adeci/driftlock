95dd5c0c26d5a305a5183c2c42156a5e49caaaa0
k-seltzer <ksetser@ufl.edu>
2025-02-18 03:04:16 -0500

    Adjusted Godot Tree Structure, Added Main Menu, Added Name Tags, Added Deleting Ports, Moved Net Code, and Added Loading Levels.

--- driftlock/scenes/worlds/demo.gd
+++ driftlock/scenes/worlds/demo.gd
@@ -1,13 +1,8 @@
 extends Node3D
 
-var multiplayer_peer = ENetMultiplayerPeer.new()
-
-const PORT = 9999
-var ADDRESS = "localhost" # Local for developemnt, TODO: Implement UPnP
-
 var peer_ids = []
+var names = []
 
-#TODO: Implement Multiplayer Menus
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,90 +13,27 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func port_forward() -> void:
-	$Network/ExternalIP.text = "FAILED"
-	
-	var upnp = UPNP.new()
-	var discover_result = upnp.discover()
-	
-	if discover_result == UPNP.UPNP_RESULT_SUCCESS:
-		if upnp.get_gateway() and upnp.get_gateway().is_valid_gateway():
-			
-			var map_result_udp = upnp.add_port_mapping(PORT, PORT, "driftlock_udp", "UDP", 0)
-			var map_result_tcp = upnp.add_port_mapping(PORT, PORT, "driftlock_tcp", "TCP", 0)
-			
-			if not map_result_udp == UPNP.UPNP_RESULT_SUCCESS:
-				map_result_udp = upnp.add_port_mapping(PORT, PORT, "", "UDP")
-			if not map_result_tcp == UPNP.UPNP_RESULT_SUCCESS:
-				map_result_tcp = upnp.add_port_mapping(PORT, PORT, "", "TCP")
-			if map_result_udp == UPNP.UPNP_RESULT_SUCCESS && map_result_tcp == UPNP.UPNP_RESULT_SUCCESS:
-				var external_ip = upnp.query_external_address()
-				ADDRESS = external_ip
-				$Network/ExternalIP.text = str(ADDRESS)
-
-func host() -> void:
-	port_forward()
-	
-	$Network/NetworkDisplay.text = "Server"
-	$NetworkMenu.visible = false
-	multiplayer_peer.create_server(PORT)
-	multiplayer.multiplayer_peer = multiplayer_peer
-	$Network/UniquePeerID.text = str(multiplayer.get_unique_id())
-	
-	add_player_character(1)
-	
-	multiplayer_peer.peer_connected.connect(
-		func(new_peer_id):
-			rpc_id(new_peer_id, "add_prev_connected_player", peer_ids)
-			rpc("add_player_character", new_peer_id)
-			
-			add_player_character(new_peer_id)
-	)
-	
-	multiplayer_peer.peer_disconnected.connect(
-		func(old_peer_id):
-			rpc("remove_player_character", old_peer_id)
-			
-			remove_player_character(old_peer_id)
-	)
-
-func join() -> void:
-	$Network/NetworkDisplay.text = "Client"
-	$NetworkMenu.visible = false
-	$IPAddress.visible = true
-	await $IPAddress.text_submitted
-	$IPAddress.visible = false
-	if not $IPAddress.text == "":
-		ADDRESS = $IPAddress.text
-	multiplayer_peer.create_client(ADDRESS, PORT)
-	multiplayer.multiplayer_peer = multiplayer_peer
-	$Network/UniquePeerID.text = str(multiplayer.get_unique_id())
-	
-	multiplayer.server_disconnected.connect(
-		func():
-			get_tree().quit()
-	)
-
-func _on_NetworkMenu_clicked(tab: int) -> void:
-	if(tab):
-		join()
-	else:
-		host()
 
 @rpc
-func add_player_character(peer_id) -> void:
+func add_player_character(peer_id, user_name = str(peer_id)) -> void:
 	peer_ids.append(peer_id)
+	if not names.find(user_name) == -1:
+		user_name = str(peer_id)
+	names.append(user_name)
 	var player_character = preload("res://scenes/game_object/player_character.tscn").instantiate()
 	player_character.set_multiplayer_authority(peer_id)
 	player_character.name = str(peer_id)
+	player_character.get_child(0).get_child(3).text = user_name
 	add_child(player_character)
 
 @rpc("reliable")
 func remove_player_character(peer_id) -> void:
+	var player_character = get_node(str(peer_id))
+	names.erase(str(player_character.get_child(0).get_child(3).text))
 	peer_ids.erase(peer_id)
-	remove_child(get_node(str(peer_id)))
+	remove_child(player_character)
 
 @rpc
-func add_prev_connected_player(peer_ids):
-	for peer_id in peer_ids:
-		add_player_character(peer_id)
+func add_prev_connected_player(rpc_peer_ids, rpc_names):
+	for index in range(rpc_peer_ids.size()):
+		add_player_character(rpc_peer_ids[index], rpc_names[index])
