bd7829b0aafe1d9870f03661757fb46508d72a49
drosabal <dmrosabal@gmail.com>
2025-04-12 14:00:29 -0400

    Fixed mover positioning, added button target functionality

--- driftlock/entities/scripts/func_button.gd
+++ driftlock/entities/scripts/func_button.gd
@@ -1,22 +1,14 @@
 @tool
 extends Area3D
 
-signal trigger()
-signal pressed()
-signal released()
-
 @export var target: String = ""
 @export var axis: Vector3 = Vector3(0, -1, 0)
 @export var depth: float = 0.8
 @export var speed: float = 8
-@export var active_time: float = 1
 
 var is_pressed: bool = false
 var base_translation: Vector3 = Vector3.ZERO
-var release_delay: float = 0.0
-var trigger_signal_delay: float = 0.0
-var press_signal_delay: float = 0.0
-var release_signal_delay: float = 0.0
+var release_delay: float = 0.1
 
 var overlaps: int = 0
 
@@ -29,8 +21,6 @@ func _func_godot_apply_properties(props: Dictionary):
 		depth = props["depth"] as float
 	if "speed" in props:
 		speed = props["speed"] as float
-	if "active_time" in props:
-		active_time = props["active_time"] as float
 
 func _init() -> void:
 	connect("body_shape_entered", body_shape_entered)
@@ -46,16 +36,13 @@ func _process(delta: float) -> void:
 func body_shape_entered(body_id, body: Node, body_shape_idx: int, self_shape_idx: int) -> void:
 	if body is StaticBody3D:
 		return
-
 	if overlaps == 0:
 		press()
-
 	overlaps += 1
 
 func body_shape_exited(body_id, body: Node, body_shape_idx: int, self_shape_idx: int) -> void:
 	if body is StaticBody3D:
 		return
-
 	overlaps -= 1
 	if overlaps == 0:
 		if release_delay == 0:
@@ -67,25 +54,16 @@ func body_shape_exited(body_id, body: Node, body_shape_idx: int, self_shape_idx:
 func press() -> void:
 	if is_pressed:
 		return
-
 	is_pressed = true
-
-	emit_trigger()
 	emit_pressed()
 
-func emit_trigger() -> void:
-	await get_tree().create_timer(trigger_signal_delay).timeout
-	trigger.emit()
-
 func emit_pressed() -> void:
-	await get_tree().create_timer(press_signal_delay).timeout
-	pressed.emit()
+	if target.is_empty():
+		return
+	GameManager.button_pressed.emit(target)
 
 func release() -> void:
 	if not is_pressed:
 		return
-
 	is_pressed = false
-
 	await get_tree().create_timer(release_delay).timeout
-	released.emit()
--- driftlock/entities/scripts/func_mover.gd
+++ driftlock/entities/scripts/func_mover.gd
@@ -4,49 +4,86 @@ extends AnimatableBody3D
 @export var targetname: String = ""
 @export var translation: Vector3 = Vector3(0, 0, 0)
 @export var speed: float = 1
+@export var active_time: float = 5
 @export var looped: bool = false
 
+enum State {READY, ACTIVE}
+var state = State.READY
+var moving: bool = false
 var start: Vector3 = Vector3.ZERO
 var end: Vector3 = Vector3.ZERO
-var direction: int = 1
+var trans_x: float = 0
+var trans_y: float = 0
+var trans_z: float = 0
+var timer: Timer = Timer.new()
 
-func _func_godot_apply_properties(props: Dictionary):
+func _func_godot_apply_properties(props: Dictionary) -> void:
 	if "targetname" in props:
 		targetname = props["targetname"] as String
 	if "translation" in props:
 		translation = props["translation"] as Vector3
 	if "speed" in props:
 		speed = props["speed"] as float
+	if "active_time" in props:
+		active_time = props["active_time"] as float
 	if "looped" in props:
 		looped = props["looped"] as bool
 
 func _init() -> void:
-	add_to_group("func_mover")
 	sync_to_physics = false
 
 func _ready() -> void:
-	GameManager.set_targetname(self, targetname)
+	GameManager.button_pressed.connect(_on_button_pressed)
+	add_child(timer)
+	timer.one_shot = true
+	timer.autostart = false
+	timer.wait_time = active_time
+	timer.timeout.connect(_on_timer_timeout)
 	start = position
-	end = start
+	end = position + translation
+	trans_x = translation.x
+	trans_y = translation.y
+	trans_z = translation.z
+	if looped:
+		moving = true
+		state = State.ACTIVE
+
+func _on_timer_timeout() -> void:
+	state = State.READY
+	if moving:
+		reverse_direction()
+	else:
+		moving = true
+
+func _on_button_pressed(target: String) -> void:
+	if (targetname == target):
+		activate()
 
 func _physics_process(delta: float) -> void:
-	position.x += direction * signf(translation.x) * minf(abs(translation.x) * delta * speed, abs(end.x - position.x))
-	position.y += direction * signf(translation.y) * minf(abs(translation.y) * delta * speed, abs(end.y - position.y))
-	position.z += direction * signf(translation.z) * minf(abs(translation.z) * delta * speed, abs(end.z - position.z))
-	if (looped and position.is_equal_approx(start)):
-		await get_tree().create_timer(1).timeout
-		play_motion()
-	if (looped and position.is_equal_approx(start + translation)):
-		await get_tree().create_timer(1).timeout
-		reverse_motion()
+	if moving:
+		if (sign(trans_x)*(position.x + trans_x * delta * speed) >= sign(trans_x)*end.x
+		and sign(trans_y)*(position.y + trans_x * delta * speed) >= sign(trans_y)*end.y
+		and sign(trans_z)*(position.z + trans_x * delta * speed) >= sign(trans_z)*end.z):
+			position = end
+			reverse_direction()
+			if not looped:
+				moving = false
+		else:
+			position.x += trans_x * delta * speed
+			position.y += trans_y * delta * speed
+			position.z += trans_z * delta * speed
 
-func use() -> void:
-	play_motion()
-
-func play_motion() -> void:
-	end = start + translation
-	direction = 1
-
-func reverse_motion() -> void:
+func reverse_direction() -> void:
+	trans_x = -trans_x
+	trans_y = -trans_y
+	trans_z = -trans_z
+	var temp: Vector3 = end
 	end = start
-	direction = -1
+	start = temp
+
+func activate() -> void:
+	if moving or state == State.ACTIVE:
+		return
+	timer.start()
+	moving = true
+	state = State.ACTIVE
