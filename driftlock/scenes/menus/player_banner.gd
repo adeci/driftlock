e89fc198814bd2656abc93d77a301f08ba266426
k-seltzer <ksetser@ufl.edu>
2025-04-20 23:49:34 -0400

    Removed experimental ping features.

--- driftlock/scenes/menus/player_banner.gd
+++ driftlock/scenes/menus/player_banner.gd
@@ -1,7 +1,6 @@
 extends MarginContainer
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if self.visible and NetworkManager.player_ping.has(self.name):
-		$PlayerInformation/PingContainer/Ping.text = "%d ms" % NetworkManager.player_ping[self.name]
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
